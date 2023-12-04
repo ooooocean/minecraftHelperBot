d028a34187ddeaed86750a861cbc05dfc1d21721
Ocean <hophuong.ocean@gmail.com>
2023-12-04 13:18:34 +0000

    main script changes to work on discord

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+# minecraftHelperBot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import json
+import random
 
+# this loads the discord library
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import Bot
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# import numpy for arrays
+import numpy as np
 
+# import csv for reading/writing to temporary DB
+from csv import writer
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# import datetime for date updates
+from datetime import datetime, timedelta
+from datetime import timezone
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# import re package to make data parsing easier
+import re
+
+# importing pandas for csv manipulation
+import pandas as pd
+
+# import time module for sleeps
+import time
+
+# this loads async
+import asyncio
+
+# math
+import math
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# allows for bot to detect all members belonging to the server.
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+intents.messages = True
+
+# Client is an object that represents a connection to Discord.
+# This handles events, tracks state and interacts with discord APIs
+client = discord.Client(intents=intents)
+bot_prefix = 'mc.'
+bot = commands.Bot(command_prefix=bot_prefix.lower(), intents=intents)
+
+# get the server
+localServer = discord.utils.get(client.guilds, id=GUILD)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('test passed')
+
+bot.run(TOKEN)
