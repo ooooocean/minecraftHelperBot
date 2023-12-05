707be868391d9e166c9854350381211bd236fbb7
Ocean <hophuong.ocean@gmail.com>
2023-12-05 02:11:35 +0000

    added nether coords converter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # minecraftHelperBot
 
 import os
+import re
 import json
 import random
 
@@ -9,6 +10,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext.commands import Bot
+
 
 # import numpy for arrays
 import numpy as np
@@ -62,5 +64,50 @@ async def on_ready():
 @bot.command()
 async def test(ctx):
     await ctx.send('test passed')
+
+# Displays list of coords from saved csv
+@bot.command()
+async def coords(ctx):
+    await ctx.send("Displaying list of coords.")
+
+
+
+@bot.command(name='convert')
+async def on_message(ctx):
+    # define function to check coords
+    def check_string_format_coords(string):
+        pattern = '-?\d+,-?\d+,-?\d+'
+        if re.match(pattern, string):
+            return (True)
+
+    print("coords command triggered")
+
+    # write the message to a variable
+    message_content = ctx.message.content
+
+    # define content to remove
+    command = bot_prefix + 'convert '
+
+    # remove command text for parsing
+    overworld_coords_text = message_content.replace(command,'')
+    overworld_coords_text = overworld_coords_text.replace(" ","")
+    print(overworld_coords_text)
+    # check if coords are in the right format
+    if check_string_format_coords(overworld_coords_text):
+        # convert coords to list
+        overworld_coords = overworld_coords_text.split(',')
+        overworld_coords = map(int, overworld_coords)
+        # iterate through list and assign to new variable
+        nether_coords = []
+        for num in overworld_coords:
+            c = int(num/8)
+            nether_coords.append(c)
+        nether_coords = map(str, nether_coords)
+        nether_coords_text = ', '.join(nether_coords)
+        await ctx.send("Nether coords are:\n" + nether_coords_text)
+    else:
+        await ctx.send("Wrong co-ordinate format. Please enter coords in the format 'x, y, z'.")
+
+
 
 bot.run(TOKEN)
