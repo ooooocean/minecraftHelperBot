a5630bfa230a90dc333bc0371374c0826953a5f3
Ocean <hophuong.ocean@gmail.com>
2023-12-05 05:33:38 +0000

    added print information for convert command

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def on_message(ctx):
         if re.match(pattern, string):
             return (True)
 
-    print("coords command triggered")
+    print("Coordinate convert command triggered.")
 
     # write the message to a variable
     message_content = ctx.message.content
@@ -75,6 +75,7 @@ async def on_message(ctx):
         nether_coords = map(str, nether_coords)
         nether_coords_text = ', '.join(nether_coords)
         await ctx.send("Nether coords are:\n" + nether_coords_text)
+        print(f"Success - converted overworld coords {overworld_coords_text} to nether coords {nether_coords_text}.")
     else:
         await ctx.send("Wrong co-ordinate format. Please enter coords in the format 'x, y, z'.")
 
