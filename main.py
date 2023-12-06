8c69558440d8fcf09096938a9b2f96cef7a55c8b
Ocean <hophuong.ocean@gmail.com>
2023-12-06 13:30:09 +0000

    added waits for map generation and delete map once complete

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import matplotlib.pyplot as plt
-import datetime
+import time
 
 # import re package to make data parsing easier
 import re
@@ -21,6 +21,11 @@ database_params = {
     "host": os.getenv('HOST'),
     "database": os.getenv('DATABASE')
 }
+
+# get root dir
+ROOT_DIR = os.path.dirname(
+    os.path.abspath(__file__)
+)
 
 # allows for bot to detect all members belonging to the server.
 intents = discord.Intents.all()
@@ -128,6 +133,7 @@ async def on_message(ctx):
     # Generating map
     plt.scatter(x_coord_list, z_coord_list,
                 marker='+')
+    print("Initial plot generated.")
 
     # add labels to data points
     for i, txt in enumerate(description_list):
@@ -148,8 +154,19 @@ async def on_message(ctx):
                     right = True,
                     direction='in')
 
+    print("Final plot completed.")
 
     plt.savefig("minecraftHelperBot/Map.png")
+    print("Starting save of plot into file.")
+
+    # define plot directory
+    PLOT_DIR = os.path.join(ROOT_DIR, "Map.png")
+
+    # check if file does not exist and if so, wait until it does
+    if os.path.exists(PLOT_DIR) is False:
+        print("File has not been generated, waiting for file to generate.")
+        while os.path.exists(PLOT_DIR) is False:
+            time.sleep(0.1)
 
     # generate embed object for display
     embed_object = discord.Embed(title="Coordinates List",
@@ -164,20 +181,16 @@ async def on_message(ctx):
                            value='\n'.join(coords_embed_list),
                            inline=True)
 
-    # get root dir
-    ROOT_DIR = os.path.dirname(
-        os.path.abspath(__file__)
-    )
-
-    # define plot directory
-    PLOT_DIR = os.path.join(ROOT_DIR,"Map.png")
-
     embed_map = discord.File(PLOT_DIR, filename="Map.png")
     embed_object.set_image(url="attachment://Map.png")
 
     embed_object.set_thumbnail(url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7c15a5ea-0f0a-4641-a73b-f504324da8ed/d4v2t3p-5bca9982-2e12-49bd-b821-d17e226b94ab.png/v1/fill/w_900,h_506,q_75,strp/minecraft_map_by_theswedishswede-d4v2t3p.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83YzE1YTVlYS0wZjBhLTQ2NDEtYTczYi1mNTA0MzI0ZGE4ZWQvZDR2MnQzcC01YmNhOTk4Mi0yZTEyLTQ5YmQtYjgyMS1kMTdlMjI2Yjk0YWIucG5nIiwid2lkdGgiOiI8PTkwMCIsImhlaWdodCI6Ijw9NTA2In1dXX0.pRk4hg347bq3tYPDGl76EuZixO5JTr_6_PG0V8vrZ64")
     await ctx.send(embed=embed_object,
                    file=embed_map)
+    print("Message sent with list of coordinates and map.")
+    # remove map file
+    os.remove(PLOT_DIR)
+    print("Map deleted.\n------")
 
 @bot.command(name='addcoords', description="Adds coordinates in the format x,y,z,<description>.")
 async def on_message(ctx):
